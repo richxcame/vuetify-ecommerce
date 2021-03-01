2db6a261fc4ae1b2ae7e28e20fb6542e12736af7
baygeldicolukov <baygeldicolukov@gmail.com>
2021-03-01 20:47:14 +0500

    Home uytgedildi

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,15 +19,82 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
 
+
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="transparent"
+            v-bind="attrs"
+            v-on="on"
+            elevation="0"
+            class="mr-2 smHide"
+          >
+          Categories
+          </v-btn>
+      </template>
+        
+        <v-list>
+          <v-list-item v-for="(item, index) in categories" :key="index" @click="onClick(item.route)">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
       <v-btn 
-        icon
-        class="mr-2"
+        color="transparent" 
+        elevation="0" 
+        class="mr-2 smHide"
       >
-        <v-icon>mdi-login</v-icon>
+        About us
       </v-btn>
+
+      <v-btn 
+        color="transparent" 
+        elevation="0" 
+        class="mr-2 smHide"
+      >
+        Contact us
+      </v-btn>
+
+      <!-- Cart icon -->
+      <span class="smHide">
+        <v-badge
+          content="2"
+          overlap
+          color="orange darken-2"
+          offset-x="28"
+          offset-y="22"
+          class="smHide"
+        >
+          <v-btn 
+          icon
+          class="mr-2 smHide"
+          >
+            <v-icon>mdi-cart-outline</v-icon>
+          </v-btn>
+        </v-badge>
+        </span>
+
+
+      <!-- Login icon -->
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn 
+          icon
+          class="mr-2"
+          v-on="on"
+          v-bind="attrs"
+          >
+            <v-icon>mdi-login</v-icon>
+          </v-btn>          
+        </template>
+        <span>Login</span>
+      </v-tooltip>
+      
     </v-app-bar>
 
-    <!-- Search -->
+
+    <!-- Search field -->
     <v-main>
         <v-container>
           <v-expand-transition>
@@ -110,13 +177,33 @@ import singleProduct from '../components/singleProduct.vue'
           { title: 'Instagram', icon: 'mdi-instagram', color: 'red lighten-1'  },
           { title: 'Linkedin', icon: 'mdi-linkedin', color: 'deep-purple'  },
           { title: 'Chat', icon: 'mdi-chat', color: 'success darken-3'  },
+        ],
+        categories:[
+          {title: 'Clothes', route: '/jackets'},
+          {title: 'Phone',  route: '/phone'},
+          {title: 'Computer',  route: '/computer'},
+          {title: 'T-shirt',  route: '/tshirt'},
+          {title: 'Shoes', route: '/shoes'},
+          {title: 'Jeans', route: 'jeans'},
+          {title: 'Watches', route: '/watches'}
         ]
       }
     },
     methods:{
       toggleSearch(){
         this.isClicked = !this.isClicked; 
+      },
+      onClick(query){
+        this.$router.push(query);
       }
     }
   };
 </script>
+
+<style>
+  @media all and (max-width: 750px){
+    .smHide{
+      display:none;
+    }
+  }
+</style>
